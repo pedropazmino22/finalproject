aab60bc5de6c1926fc0ec679bc0b579529a21606
pedropazmino22 <143547421+pedropazmino22@users.noreply.github.com>
2024-04-16 17:12:42 -0400

    Add files via upload

---
+++ js/javascript2.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('contactForm');
+const resetBtn = document.getElementById('resetBtn');
+
+resetBtn.addEventListener('click', function () {
+    form.reset();
+});
+
